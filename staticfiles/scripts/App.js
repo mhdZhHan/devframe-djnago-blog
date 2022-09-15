32ee75b8e3375bd3a427883497ec170238ba8cb9
Mohammed Shajahan <mohammedsh6236@gmail.com>
2022-09-15 23:44:51 +0530

    Fix staticfiles

--- staticfiles/scripts/App.js
+++ staticfiles/scripts/App.js
@@ -1,15 +1,10 @@
 window.addEventListener('load', ()=>{
-    const searchIcon = document.querySelector('#search_toggle')
+    const searchIcon = document.querySelector('#search_toggle') || null
     const searchBox = document.querySelector('#search_box')
 
     const mobileMenu = document.querySelector('.mobile_menu_container')
     const mobileMenuIcon = document.querySelector('.mobile_menu_icon')
     const menuCloseIcon = document.querySelector('.menu_close_icon')
-
-    // search toggle
-    searchIcon.addEventListener('click', ()=>{
-        searchBox.classList.toggle('active')
-    })
 
     // mobile menu
     mobileMenuIcon.addEventListener('click', ()=>{
@@ -19,4 +14,11 @@ window.addEventListener('load', ()=>{
     menuCloseIcon.addEventListener('click', ()=>{
         mobileMenu.classList.remove('active')
     })
+
+    // search toggle
+    if(searchIcon !== null){
+        searchIcon.addEventListener('click', ()=>{
+            searchBox.classList.toggle('active')
+        })    
+    }
 })
