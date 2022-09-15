a2f860d64c4a06a249b429761221c0b0eb6cf208
Mohammed Shajahan <mohammedsh6236@gmail.com>
2022-09-15 22:10:03 +0530

    Add postgresql db

--- devframe/settings.py
+++ devframe/settings.py
@@ -1,16 +1,10 @@
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ke5h1g=b!0wbv96q9=q%^^_34i)(f4!cfk*z)js$&o1e+zgahv'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['devframe-django-blog.herokuapp.com', '127.0.0.1']
@@ -63,18 +57,26 @@ WSGI_APPLICATION = 'devframe.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd5f3b2no9nvpmc',
+        'USER': 'dowqqxpuesbvfg',
+        'PASSWORD': 'a716c3c358e6dff783f23ed35e51c183bcb5defec32fea9ac79800cdd797727f',
+        'HOST': 'ec2-44-207-133-100.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
-
 # Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
